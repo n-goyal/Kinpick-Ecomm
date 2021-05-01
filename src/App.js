b5bbade87c927dfd185f3b7560f08c14b7c0603b
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-01 08:24:07 +0530

    test advanced routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+// import Shop from "./tempshop-comps/tempShop.page";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        {/* withRouter( */}
         <Route path="/shop" component={Shop} />
+        {/* ) */}
       </Switch>
     </div>
   );
