f49b6d31095ecde77f35fbcfdf2ea1315cfb24f7
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-01 17:00:09 +0530

    add banner component

---
+++ src/components/banner/banner.component.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./banner.styles.scss";
+import { LinkButton } from "../link-button/link-button.component";
+
+export const Banner = ({
+  headline,
+  preamble,
+  links,
+  headlineColor,
+  bgColor,
+}) => {
+  return (
+    <section style={{ backgroundColor: `${bgColor}` }} className="banner">
+      <div className="banner-container">
+        <article className="banner-content">
+          <h2 style={{ color: `${headlineColor}` }}>{headline}</h2>
+          <p style={{ color: `${headlineColor}` }}>{preamble}</p>
+        </article>
+        <div className="links-container">
+          {links.map((link) => {
+            return (
+              <div className="link">
+                <LinkButton
+                  routeName={link.routeName}
+                  buttonColor={link.bgColor}
+                  buttonLabel={link.label}
+                  labelColor={link.color}
+                  size={link.size}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Banner } from "../../components/banner/banner.component";
 import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 export const Homepage = () => {
+  const bannerDetails = {
+    headline: "Get Comfy : Buy 3 basics & get 20% off",
+    preamble:
+      "Valid on hm.com until 5th May 2021 23:59pm or while stock lasts.",
+    headlineColor: "white",
+    bgColor: "#bdb2ff",
+    links: [
+      {
+        label: "Women",
+        color: "white",
+        bgColor: "black",
+        size: "small",
+        routeName: "/somewhere/to/go",
+      },
+      {
+        label: "Men",
+        color: "white",
+        bgColor: "black",
+        size: "small",
+        routeName: "/somewhere/to/go",
+      },
+      {
+        label: "Kids",
+        color: "white",
+        bgColor: "black",
+        size: "small",
+        routeName: "/somewhere/to/go",
+      },
+    ],
+  };
   return (
     <div className="homepage">
+      <Banner {...bannerDetails} />
       <Directory />
     </div>
   );
