70a9ef98865938e760eb706cd589ba492d9a5cde
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-04 23:41:00 +0530

    add sign-in sign-out listener

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, withRouter } from "react-router-dom";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import "./App.css";
 // import { Header as HeaderNew } from "./components/header/header.component";
 import { Homepage } from "./pages/homepage/homepage.component";
@@ -8,20 +8,55 @@ import { SignInSignUp } from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.co
 import { Header } from "./tempshop-comps/header.comp";
 // import Shop from "./tempshop-comps/tempShop.page";
 
-function App() {
-  return (
-    <div>
-      {/* <HeaderNew /> */}
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        {/* withRouter( */}
-        <Route path="/shop" component={Shop} />
-        {/* ) */}
-        <Route path="/signin" component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({
+        user: user,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    console.log(this.state.user);
+    return (
+      <div>
+        {/* <HeaderNew /> */}
+        <Header user={this.state.user} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          {/* withRouter( */}
+          <Route path="/shop" component={Shop} />
+          {/* ) */}
+          {this.state.user ? (
+            <Redirect to="/" />
+          ) : (
+            <Route
+              path="/signin"
+              user={this.state.user}
+              component={SignInSignUp}
+            />
+          )}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
