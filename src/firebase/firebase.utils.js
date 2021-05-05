883865a7bf304d5d276ff726c5c9f29d55919f90
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-05 18:59:01 +0530

    add user to firestore database functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,6 +16,35 @@ export const app = firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = app.firestore();
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // query firestory if user exists
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // document ref
+  const snapshot = await userRef.get(); // check if data exists
+
+  console.log(`does user already exists? ${snapshot.exists}`);
+
+  if (!snapshot.exists) {
+    // create user
+    const { displayName, email } = userAuth;
+    const createdAt = Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error(`Error while creating the user: ${error}`);
+    }
+  }
+
+  return userRef;
+};
+
 // google authentication
 const provider = new firebase.auth.GoogleAuthProvider();
 
