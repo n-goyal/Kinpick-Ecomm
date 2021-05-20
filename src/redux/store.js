a9701b01bf086c57eb806b43fde02bd9057bafef
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-20 20:09:13 +0530

    Fixed setup bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root.reducer";
-import userReducer from "./user/user.reducer";
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default userReducer;
+export default store;
