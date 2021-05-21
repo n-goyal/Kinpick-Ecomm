b871833f35b135c5120ad15b0108ca379a6cc964
nitin kumar goyal <goyalnitin634@gmail.com>
2021-05-21 08:30:13 +0530

    Updated App Redirect to home if already user is signed in

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
 import "./App.css";
-import { Header } from "./components/header/header.component";
+
+import Header from "./components/header/header.component";
 import { Homepage } from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
-
 import SignUp from "./components/sign-up/sign-up.component";
 import SignIn from "./components/sign-in/sign-in.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      user: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    console.log(this.props);
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            user: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       }
-      this.setState({
-        user: userAuth,
-      });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -48,20 +42,32 @@ class App extends React.Component {
     console.log(this.state);
     return (
       <div>
-        <Header user={this.state.user} />
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop/en_in/:type" component={Shop} />
-          {this.state.user ? (
-            <Redirect to="/" />
-          ) : (
-            <Route path="/signin" user={this.state.user} component={SignIn} />
-          )}
-          <Route path="/signup" user={this.state.user} component={SignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() => (this.props.user ? <Redirect to="/" /> : <SignIn />)}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={() => (this.props.user ? <Redirect to="/" /> : <SignUp />)}
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  user: state.user.user,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,13 +1,17 @@
+import { userActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
-  currentUser: null,
+  user: null,
 };
+
+const { SET_CURRENT_USER } = userActionTypes;
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case SET_CURRENT_USER:
       return {
         ...state,
-        currentUser: action.payload,
+        user: action.payload,
       };
     default:
       return state;
