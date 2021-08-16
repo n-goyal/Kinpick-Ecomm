e0909453c459eb530bba90b50cec0ed71179a78b
nitin kumar goyal <goyalnitin634@gmail.com>
2021-08-16 09:14:25 +0530

    fix image rendering in other product components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import {
-  moveItemToWishlist,
-} from "../../redux/cart/cart.actions";
+import { moveItemToWishlist } from "../../redux/cart/cart.actions";
 
 import RemoveIconContainer from "../../containers/removeIconContainer/RemoveIconContainer";
 
@@ -11,33 +9,41 @@ import "./cart-item.styles.scss";
 
 const CartItem = ({ item, moveItemToWishlist }) => {
   const { name, image1url, price, quantity } = item;
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
   console.log(item.id);
   return (
     <div className="cart-item">
-      <img src={image1url} alt={name} />
-      <div className="item-details">
-        <span className="name">{name} </span>
-        <span className="name">
-          {quantity} X {price}
-        </span>
-      </div>
-      <div
-        className="icon-container"
-        onClick={() => {
-          moveItemToWishlist(item);
-        }}
-      >
-        <svg
-          className="icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" />
-        </svg>
-      </div>
-      <RemoveIconContainer item={item} itemType="cart" />
+      <img src={image1url} alt={name} onLoad={() => setIsLoaded(true)} />
+      {isLoaded ? (
+        <>
+          <div className="item-details">
+            <span className="name">{name} </span>
+            <span className="name">
+              {quantity} X {price}
+            </span>
+          </div>
+          <div
+            className="icon-container"
+            onClick={() => {
+              moveItemToWishlist(item);
+            }}
+          >
+            <svg
+              className="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" />
+            </svg>
+          </div>
+          <RemoveIconContainer item={item} itemType="cart" />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- src/components/wishlist-item/wishlist-item.component.jsx
+++ src/components/wishlist-item/wishlist-item.component.jsx
@@ -19,62 +19,73 @@ const WishlistItem = ({
   moveItemWishlistToCart,
   removeItemFromWishlist,
 }) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
   // modified product card component - styles are same as that of product card
   return (
     <div className="item-sizing">
       <div className="">
         <div className="image-container">
           <Link to="/:productName">
-            <img src={item.image1url} alt={item.name} />
+            <img
+              src={item.image1url}
+              alt={item.name}
+              onLoad={() => setIsLoaded(true)}
+            />
           </Link>
         </div>
-        <div className="item-details">
-          <div className="item-price">
-            <div className="item-heading">{item.name}</div>
-            <div>
-              <span>{item.price}</span>
+        {isLoaded ? (
+          <>
+            <div className="item-details">
+              <div className="item-price">
+                <div className="item-heading">{item.name}</div>
+                <div>
+                  <span>{item.price}</span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="actions">
-          <CustomButton
-            onClick={() => {
-              // add to cart
-              moveItemWishlistToCart(item);
-            }}
-            bgColor="white"
-            bdColor="black"
-            color="black"
-            className="button"
-            onclick="document.body.style.backgroundColor = 'green';"
-          >
-            <div className="icon-text">
-              <svg
-                class="icon"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="actions">
+              <CustomButton
+                onClick={() => {
+                  // add to cart
+                  moveItemWishlistToCart(item);
+                }}
+                bgColor="white"
+                bdColor="black"
+                color="black"
+                className="button"
+                onclick="document.body.style.backgroundColor = 'green';"
               >
-                <path d="M12 .1c2.9 0 4.9 1.4 4.9 3.8v.6h3.8v5.1c0 4.7 2.1 13.3.5 13.3-.7 0-5 1.1-9.2 1.1-4.6 0-9.2-1.1-9.2-1.1-1.5 0 .5-8.7.5-13.3V4.4h3.8v-.6c0-2.4 2-3.7 4.9-3.7zm7.6 5.4h-2.7v2.2h-1.1V5.5H8.2v2.2H7.1V5.5H4.4v4c0 1.3-.1 2.9-.4 5.3l-.1.9c-.4 3-.5 3.9-.5 5v1.2c.8.1 1.6.3 2.4.4 2.1.3 4.2.5 6.1.5h.5c1.6 0 3.3-.2 5.2-.5.2 0 2-.4 2.9-.5v-1.4c0-1-.2-1.9-.5-4.7 0-.3-.1-.6-.1-.9-.3-2.4-.4-4-.4-5.3v-4zM12 1.2c-2.3 0-3.8 1-3.8 2.6v.6h7.6v-.6c0-1.7-1.5-2.6-3.8-2.6z"></path>
-              </svg>
-              <span className="text">Add</span>
-            </div>
-          </CustomButton>
+                <div className="icon-text">
+                  <svg
+                    class="icon"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M12 .1c2.9 0 4.9 1.4 4.9 3.8v.6h3.8v5.1c0 4.7 2.1 13.3.5 13.3-.7 0-5 1.1-9.2 1.1-4.6 0-9.2-1.1-9.2-1.1-1.5 0 .5-8.7.5-13.3V4.4h3.8v-.6c0-2.4 2-3.7 4.9-3.7zm7.6 5.4h-2.7v2.2h-1.1V5.5H8.2v2.2H7.1V5.5H4.4v4c0 1.3-.1 2.9-.4 5.3l-.1.9c-.4 3-.5 3.9-.5 5v1.2c.8.1 1.6.3 2.4.4 2.1.3 4.2.5 6.1.5h.5c1.6 0 3.3-.2 5.2-.5.2 0 2-.4 2.9-.5v-1.4c0-1-.2-1.9-.5-4.7 0-.3-.1-.6-.1-.9-.3-2.4-.4-4-.4-5.3v-4zM12 1.2c-2.3 0-3.8 1-3.8 2.6v.6h7.6v-.6c0-1.7-1.5-2.6-3.8-2.6z"></path>
+                  </svg>
+                  <span className="text">Add</span>
+                </div>
+              </CustomButton>
 
-          <CustomButton
-            onClick={() => {
-              removeItemFromWishlist(item);
-            }}
-            bgColor="white"
-            bdColor="black"
-            color="black"
-            className="button"
-          >
-            <div className="icon-text">
-              <RemoveIconContainer item={item} itemType="wishlist" />
+              <CustomButton
+                onClick={() => {
+                  removeItemFromWishlist(item);
+                }}
+                bgColor="white"
+                bdColor="black"
+                color="black"
+                className="button"
+              >
+                <div className="icon-text">
+                  <RemoveIconContainer item={item} itemType="wishlist" />
+                </div>
+              </CustomButton>
             </div>
-          </CustomButton>
-        </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
